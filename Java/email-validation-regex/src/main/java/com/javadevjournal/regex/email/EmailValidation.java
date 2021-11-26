f970d0a5ff5f370aaac6a6ac447367bb20d3e214
Hesam Rasouli <hesam.rasouli1@gmail.com>
2021-11-27 01:00:08 +0330

    Delete Java directory

--- Java/email-validation-regex/src/main/java/com/javadevjournal/regex/email/EmailValidation.java
+++
@@ -1,6 +0,0 @@
-package com.javadevjournal.regex.email;
-
-public interface EmailValidation {
-
-    boolean validateEmailAddress(final String email);
-}
--- Java/javadeepcopy/src/main/java/com/javadevjournal/CopyConstructor.java
+++
@@ -1,4 +0,0 @@
-package com.javadevjournal;
-
-public class CopyConstructor {
-}
