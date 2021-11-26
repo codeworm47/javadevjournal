6805536e878b2abc927a601f14edfbc23fd68fb9
Hesam Rasouli <hesam.rasouli1@gmail.com>
2021-11-27 01:01:27 +0330

    Delete spring directory

--- spring/rest-pagination/src/main/java/com/javadevjournal/rest/jpa/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javadevjournal.rest.jpa;
-
-import com.javadevjournal.rest.entity.ProductEntity;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ProductRepository extends PagingAndSortingRepository<ProductEntity,Integer> {
-}
