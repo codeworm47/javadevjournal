af3245b6989429b31a87432761d6649ce272ab37
Hesam Rasouli <hesam.rasouli1@gmail.com>
2021-11-27 01:00:36 +0330

    Delete java-8 directory

--- java-8/Java-method-reference/NameComparator.java
+++
@@ -1,9 +0,0 @@
-public class NameComparator {
-    public int compareNames(Student s1, Student s2){
-        return s1.getName().compareTo(s2.getName());
-    }
-
-    public Student compareMarks(Student s1,Student s2){
-        return s1.getMarks() > s2.getMarks() ? s1 : s2;
-    }
-}
